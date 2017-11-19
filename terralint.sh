6ed0dc1a79f6ff7197ff465159f2688763d18015
Leo Blanc <leo@strataconsulting.com>
2017-11-19 12:11:01 -0300

    Added more logging

--- terralint.sh
+++ terralint.sh
@@ -2,11 +2,17 @@
 
 set -x
 
+LOGFILE=/tmp/terralint.log
+
 # TerraLint doest work file by file. It analize the entire directory
 # Therefore, take the base directory of the first file as argument
 FILE1=$1
 DIRECTORY=$(dirname "${FILE1}")
 
+echo "FILE1=$FILE1" >> /tmp/terralint.log
+echo "DIRECTORY=$DIRECTORY" >> /tmp/terralint.log
+
 # Execute terralint.py providing the directory where the terraform file(s) are located
 SCRIPTDIR=$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )
-python "${SCRIPTDIR}/terralint.py" "${DIRECTORY}"
+echo "SCRIPTDIR=$SCRIPTDIR" >> /tmp/terralint.log
+python "${SCRIPTDIR}/terralint.py" "${DIRECTORY}" >> /tmp/terralint.log
