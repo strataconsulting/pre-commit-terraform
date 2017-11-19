3d49ab632786defccb2524de80611082eff0b25b
Leo Blanc <leo@strataconsulting.com>
2017-11-19 12:46:16 -0300

    Removed logging info

--- terralint.sh
+++ terralint.sh
@@ -1,18 +1,10 @@
 #!/usr/bin/env bash
 
-set -x
-
-LOGFILE=/tmp/terralint.log
-
 # TerraLint doest work file by file. It analize the entire directory
 # Therefore, take the base directory of the first file as argument
 FILE1=$1
 DIRECTORY=$(dirname "${FILE1}")
 
-echo "FILE1=$FILE1" >> /tmp/terralint.log
-echo "DIRECTORY=$DIRECTORY" >> /tmp/terralint.log
-
 # Execute terralint.py providing the directory where the terraform file(s) are located
 SCRIPTDIR=$(dirname "$0")
-echo "SCRIPTDIR=$SCRIPTDIR" >> /tmp/terralint.log
-python "${SCRIPTDIR}/terralint.py" "${DIRECTORY}" >> /tmp/terralint.log
+python "${SCRIPTDIR}/terralint.py" "${DIRECTORY}"
